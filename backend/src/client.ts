d3ef7093b89c0a1bd95a9e3f9b4ebd6549512874
Cody Wang <codywang999@gmail.com>
2023-03-05 13:11:06 -0700

    forgot to add frontend

--- backend/src/client.ts
+++ backend/src/client.ts
@@ -1,3 +1,4 @@
+import { providers } from "ethers";
 import { LoadBalancerProvider } from "../library/lb";
 
 const loadBalancer = new LoadBalancerProvider(
@@ -8,6 +9,8 @@ const loadBalancer = new LoadBalancerProvider(
 (async () => {
   let i = 20;
   while (i > 0) {
+    // const provider = new providers.JsonRpcProvider("<URL>");
+    // const block = await provider.getBlock(1);
     const output = await loadBalancer.providerCall("eth_getBlock", ["0x1"]);
     console.log(JSON.parse(await output.json()));
     i--;
